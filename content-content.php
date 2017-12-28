3da2ba1d6c400e0073bc14cb577a4ff0378f63bf
Giang, Thai Nguyen Minh <14520231@gm.uit.edu.vn>
2017-12-28 15:13:11 +0700

    add float left for company css

--- content-content.php
+++ content-content.php
@@ -14,7 +14,7 @@ Time: 11:37 AM-->
                 ?>
                 <div class="item">
                     <div class="company-detail">
-                        <img src="<?php echo $row_all['company_logo_link']; ?>" style="width: 100px">
+                        <img src="<?php echo $row_all['company_logo_link']; ?>" style="width: 100px;">
                         <div>
                             <a href="<?php echo $row_all['company_link']; ?>">Tên công ty: <?php echo $row_all['company_name']; ?></a>
                             <p>Địa chỉ: <?php echo $row_all['address']; ?></p>
